1b4d64b062de5d1f16bdb9a3da3f347df38ce0a8
Rolf Madsen <rolfrm@gmail.com>
2023-02-28 04:23:06 +0300

    parser!

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use std::io::Read;
 use std::file;
 use std::fmt;
 use std::collections::HashMap;
+use std::str::FromStr;
 
 enum LispValue{
-    Cons{car: Box<LispValue>, cdr: Box<LispValue>},
+    Cons(Box<(LispValue, LispValue)>),
     Nil,
     String(String),
     Rational(f64),
@@ -19,8 +20,7 @@ enum LispValue{
 impl LispValue {
     
     fn cons(car: LispValue, cdr: LispValue) -> LispValue {
-        return LispValue::Cons{car: Box::new(car), cdr: Box::new(cdr)};
-
+        return LispValue::Cons(Box::new((car, cdr)));
     }
 }
 
@@ -53,15 +53,17 @@ impl TryInto<i64> for LispValue{
 impl fmt::Display for LispValue {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match &*self {
-            LispValue::Cons { car, cdr } =>{
+            LispValue::Cons (bx)=>{
+                
                 write!(f, "(").unwrap();
-                write!(f, "{}", car).unwrap();
-                if let LispValue::Nil = cdr.as_ref() {
+                write!(f, "{}", bx.0).unwrap();
+                if let LispValue::Nil = bx.1 {
                     return write!(f, ")");    
                 }
                 write!(f, " ").unwrap();
-                write!(f, "{}", cdr).unwrap();
+                write!(f, "{}", bx.1).unwrap();
                 return write!(f, ")");
+                
             }
             LispValue::Nil => {
                 return write!(f, "()");
@@ -72,7 +74,8 @@ impl fmt::Display for LispValue {
             LispValue::Symbol(id) => {
                 return write!(f, "Symbol({})", id)
             }
-            &LispValue::Rational(_) | &LispValue::Integer(_) => todo!()
+            &LispValue::Rational(x) => {write!(f, "{0}", x)}
+            LispValue::Integer(x) => {write!(f, "{0}", x)}
 
         }
     
@@ -81,7 +84,7 @@ impl fmt::Display for LispValue {
 
 struct LispContext{
     symbols: HashMap<String, i64>,
-    id_gen: i64
+    id_gen: i64,
 }
 
 impl<'a> LispContext{
@@ -90,28 +93,130 @@ impl<'a> LispContext{
         return x
     }
 
-    fn new2() -> HashMap<&'a str, i64> {
+    fn new2() -> HashMap<String, i64> {
         HashMap::new()
     }
 
-    fn NewSymbol(& mut self, name: String) -> LispValue{
+    fn NewSymbol(& mut self, name: &str) -> LispValue{
+        
 
-        if let Option::Some(id) = self.symbols.get(&name) {
+        if let Option::Some(id) = self.symbols.get(name) {
             return LispValue::Symbol(*id);
         }
         let id2  = self.id_gen;
         self.id_gen += 1;
-        //let name2 = name.clone();
-        self.symbols.insert(name.to_string(), id2);
+
+        self.symbols.insert(name.into(), id2);
         return LispValue::Symbol(id2)
+    }
+}
+
+fn parse_integer<'a>(code0: &'a [u8], val: &mut i64) -> Option<&'a [u8]>{
+    *val = 0;
+    let mut code = code0;
+    while code.len() > 0 &&  code[0] >= b'0' && code[0] <= b'9' {
+        *val = *val * 10;
+        let charcode = code[0] - b'0';
+        *val = *val + i64::from(charcode);
+        code = &code[1..]
 
     }
+    if code.len() > 0 && code[0] != b')' && code[0] != b' '{
+        return None
+    }
+    return Option::Some(code);
+}
+fn skip_whitespace(code: &[u8]) -> &[u8]{
+    let mut code2 = code;
+    while(code2.len() > 0 && code2[0] == b' '){
+        code2 = &code2[1..];
+    }
+    return code2;
+}
+
+fn parse_symbol<'a>(ctx: &mut LispContext, code0: &'a [u8], value: &mut LispValue) ->  Option<&'a[u8]>{
+    
+    let mut code = code0;
+    let mut len = 0;
+    
+    while code.len() > 0 &&  code[0] != b')' && code[0] != b' ' {
+        code = &code[1..];
+        len += 1;
+    }
+    
+    if code.len() > 0 && code[0] != b')' && code[0] != b' '{
+        return None
+    }
+
+    if let Ok(str) = String::from_utf8(Vec::from(&code0[..len])){
+        *value = ctx.NewSymbol(&str.to_string()) ;
+    
+    }
+    
+    return Option::Some(code);
 
 }
 
+
+fn parse<'a>(ctx: &mut LispContext, code: &'a [u8], value: &mut LispValue) ->  Option<&'a[u8]>{
+    let mut code2 = code;
+    code2 = skip_whitespace(code2);
+
+    let mut integerval: i64 = 0;
+    if let Some(code3) = parse_integer(code2, &mut integerval)  {
+        *value = LispValue::Integer(integerval);
+        return Option::Some(code3);
+    }
+    if code2.len() == 0 {
+        return None;
+    }
+
+    if(code2[0] == b'('){
+        code2 = &code2[1..];
+        
+        code2 = skip_whitespace(code2);
+        let mut sub = Vec::new();
+        while code2.len() > 0 && code2[0] != b')'{
+            
+
+            println!("{}", char::from(code2[0]));
+            let mut v2 = LispValue::Nil;
+            if let Some(x) = parse(ctx, code2, &mut v2) {
+                sub.push(v2);
+                code2 = x;
+            }
+            
+            code2 = skip_whitespace(code2);
+
+        }
+
+        let mut v = LispValue::Nil;
+        while sub.len() > 0{
+            v = LispValue::Cons( Box::new((sub.pop().unwrap(), v)))
+        }
+        *value = v;
+        return Some(code2);
+    }
+
+    if code2[0] == b'"'{
+        code2 = &code2[1..];
+        return None;
+    }
+
+    {
+        let mut v = LispValue::Nil;
+        
+        if let Some(sym) = parse_symbol(ctx, code2, value) {
+            println!("Parsed  symbol!");
+            return Some(sym)
+        }
+    }
+
+    return Some(code2);
+}
+
 fn main() {
-    println!("Hello, world!");
-    let code = "(+ 1 2)";
+    let mut ctx = LispContext::new();println!("Hello, world!");
     let nil = LispValue::Nil;
     println!("{}", nil);
     let X = "Hej";
@@ -120,13 +225,24 @@ fn main() {
     let x2 : i64 = LispValue::from(123).try_into().unwrap();
     let v1 = LispValue::cons(str1, LispValue::cons(str2, LispValue::Nil));
     println!("{}", v1);
-    let f = BufReader::new(code.as_bytes());
+    
+    
+    let a = ctx.NewSymbol("1234");
+    let b = ctx.NewSymbol("123");
 
-    let mut ctx = LispContext::new();
-    let a = ctx.NewSymbol("1234".to_string());
-    let b = ctx.NewSymbol("123".to_string());
-    let c = ctx.NewSymbol("123".to_string());
+{
+    let alice = String::from("I like dogs");
+    let d = ctx.NewSymbol(&alice);
+
+}
+
+    let c = ctx.NewSymbol("123");
 
     println!("{} {} {}", a, b, c);
+
+    let code = "(111 222 333 asd)";//"(+ 1 2)";
+    let mut out : LispValue = LispValue::Nil;
+    let code2 = parse(&mut ctx, code.as_bytes(), &mut out);
+    println!("Parsed: {}", out);
 
 }
