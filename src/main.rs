a09eec2e726b1fa735b2219bdbd71ccb01ba71e4
Rolf Madsen <rolfrm@gmail.com>
2023-03-02 19:28:00 +0300

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use lisp::*;
 mod math;
 use math::*;
 
+mod parser;
+use parser::*;
+
 #[derive(Clone)]
 pub enum LispValue{
     Cons(Box<(LispValue, LispValue)>),
@@ -178,7 +181,7 @@ impl fmt::Display for LispValue {
     }
 }    
 
-struct LispContext{
+pub struct LispContext{
     symbols: HashMap<String, i64>,
     id_gen: i64,
     globals: Vec<LispValue>,
@@ -190,7 +193,7 @@ impl<'a> LispContext{
         return LispContext{symbols: HashMap::new(), id_gen: 1, globals: Vec::new(), global_names: HashMap::new()};
     }
 
-    fn get_symbol_id(& mut self, name: &str) -> i64{
+    pub fn get_symbol_id(& mut self, name: &str) -> i64{
         
 
         if let Option::Some(id) = self.symbols.get(name) {
@@ -203,11 +206,11 @@ impl<'a> LispContext{
         return id2
     }
 
-    fn get_symbol(& mut self, name: &str) -> LispValue{
+    pub fn get_symbol(& mut self, name: &str) -> LispValue{
         return LispValue::Symbol(self.get_symbol_id(name))
     }
 
-    fn set_global(&mut self, symbol_name: i64, value: LispValue){
+    pub fn set_global(&mut self, symbol_name: i64, value: LispValue){
         
         if let Option::Some(index) = self.global_names.get(&symbol_name) {
             self.globals[*index] = value;
@@ -219,7 +222,7 @@ impl<'a> LispContext{
         }
     }
 
-    fn get_global(&self, symbol_name: i64) -> Option<&LispValue>{
+    pub fn get_global(&self, symbol_name: i64) -> Option<&LispValue>{
         if let Option::Some(index) = self.global_names.get(&symbol_name) {
             return Some(&self.globals[*index]);
         }
@@ -227,160 +230,12 @@ impl<'a> LispContext{
         
     }
 
-    fn set_global_str(& mut self, name: &str, value: LispValue){
+    pub fn set_global_str(& mut self, name: &str, value: LispValue){
         let id = self.get_symbol_id(name);
         self.set_global(id, value);
     }
 }
 
-fn parse_integer<'a>(code0: &'a [u8], val: &mut i64) -> Option<&'a [u8]>{
-    *val = 0;
-    let mut code = code0;
-    while code.len() > 0 &&  code[0] >= b'0' && code[0] <= b'9' {
-        *val = *val * 10;
-        let charcode = code[0] - b'0';
-        *val = *val + i64::from(charcode);
-        code = &code[1..]
-
-    }
-    if code.len() > 0 && code[0] != b')' && code[0] != b' '{
-        return None
-    }
-    return Option::Some(code);
-}
-
-fn skip_whitespace(code: &[u8]) -> &[u8]{
-    let mut code2 = code;
-    while code2.len() > 0 && code2[0] == b' '{
-        code2 = &code2[1..];
-    }
-    return code2;
-}
-
-fn parse_rational<'a>(code0: &'a [u8], val: &mut f64) -> Option<&'a [u8]> {
-    *val = 0.0;
-    let mut code = code0;
-    while code.len() > 0 &&  code[0] >= b'0' && code[0] <= b'9' {
-        *val = *val * 10.0;
-        let charcode = code[0] - b'0';
-        *val = *val + f64::from(charcode);
-        code = &code[1..]
-    }
-    
-    
-    if code.len() > 0 && code[0] == b'.' {
-        code = &code[1..];
-        let mut subscale = 0.1;
-        while code.len() > 0 &&  code[0] >= b'0' && code[0] <= b'9' {
-            let charcode = f64::from(code[0] - b'0');
-            *val += subscale * charcode;
-            subscale *= 0.1;
-            code = &code[1..]
-    
-        }
-        return Some(code);    
-    }else{
-        // there is no '.' -> not a rational.
-    }
-    
-    return None;
-
-    
-
-}
-
-fn parse_symbol<'a>(ctx: &mut LispContext, code0: &'a [u8], value: &mut LispValue) ->  Option<&'a[u8]>{
-    
-    let mut code = code0;
-    let mut len = 0;
-    
-    while code.len() > 0 &&  code[0] != b')' && code[0] != b'(' && code[0] != b' ' {
-        code = &code[1..];
-        len += 1;
-    }
-    
-    if code.len() > 0 && code[0] != b')' && code[0] != b' ' && code[0] != b'('{
-        return None
-    }
-
-    if let Ok(str) = String::from_utf8(Vec::from(&code0[..len])){
-        *value = ctx.get_symbol(&str.to_string()) ;
-    
-    }
-    
-    return Option::Some(code);
-
-}
-
-
-fn parse<'a>(ctx: &mut LispContext, code: &'a [u8], value: &mut LispValue) ->  Option<&'a[u8]>{
-    let mut code2 = code;
-    code2 = skip_whitespace(code2);
-
-    let mut integer_value: i64 = 0;
-    if let Some(code3) = parse_integer(code2, &mut integer_value)  {
-        *value = LispValue::Integer(integer_value);
-        return Option::Some(code3);
-    }
-    let mut rational_value: f64 = 0.0;
-    if let Some(code3) = parse_rational(code2, &mut rational_value)  {
-        *value = LispValue::Rational(rational_value);
-        return Option::Some(code3);
-    }
-
-
-    if code2.len() == 0 {
-        return None;
-    }
-
-    if(code2[0] == b'('){
-        code2 = &code2[1..];
-        
-        code2 = skip_whitespace(code2);
-        let mut sub = Vec::new();
-        while code2.len() > 0 && code2[0] != b')'{
-            
-            let mut v2 = LispValue::Nil;
-            if let Some(x) = parse(ctx, code2, &mut v2) {
-                sub.push(v2);
-                code2 = x;
-            }
-            
-            code2 = skip_whitespace(code2);
-
-        }
-        code2 = &code2[1..]; 
-
-        let mut v = LispValue::Nil;
-        while sub.len() > 0{
-            v = LispValue::Cons( Box::new((sub.pop().unwrap(), v)))
-        }
-        *value = v;
-        return Some(code2);
-    }
-
-    if code2[0] == b'"'{
-        code2 = &code2[1..];
-        return None;
-    }
-
-    {
-        let mut v = LispValue::Nil;
-        
-        if let Some(sym) = parse_symbol(ctx, code2, value) {
-            return Some(sym)
-        }
-    }
-
-    return Some(code2);
-}
-
-fn parse_string(ctx: &mut LispContext, code: &str) -> LispValue{
-    let mut c = code.as_bytes();
-    let mut v = LispValue::Nil;
-    parse(ctx, c, &mut v);
-    return v;
-}
 
 
 
